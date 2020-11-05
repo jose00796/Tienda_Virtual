49dc6e6cb18dfb5b02d9b5b5710402874706731c
jose00796 <josebalboa11@hotmail.com>
2020-11-05 12:41:12 -0400

    Cumlminacion Enviar Datos al Controlador Guapo

---
+++ Config/Config.php
@@ -0,0 +1,5 @@
+<?php
+
+const BASE_URL = "http://localhost/Recuperacion/Tienda_Virtual/";
+const LIBS = "Libraries/";
+const VIEWS = "Views/";
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 //Es como una especie de condicional, sino se manda ningun dato en la url usara home/home por defecto.
 //Explode() Captura toda la cadena y el "/" actua como Delimitador para Separar.
 
+require_once ("Config/Config.php");
+
 $url = !empty($_GET['url']) ? $_GET['url'] : 'home/home'; 
 $arrUrl = explode("/", $url);
 $controller = $arrUrl[0];
@@ -28,9 +30,35 @@ if (!empty($arrUrl[2])) {
     }
 }
 
-echo "<br>";
+spl_autoload_register(function($class){ 
+        if (file_exists(LIBS.'Core'.$class.".php")) {
+            require_once (LIBS.'Core'.$class.".php");
+        }
+});
+
+//LOAD.PHP 
+
+$controller_file = "Controllers/".$controller.".php";
+
+if (file_exists($controller_file)) {
+    
+    require_once($controller_file);
+    $controller = new $controller();
+
+   if (method_exists($controller, $method)) {
+       $controller->{$method}($params);
+   } else{
+        echo "METODO NO EXISTE";
+   }
+
+}else{
+    echo $controller_file." = ". "NO EXISTE EN EL DIRECTORIO";
+}
+
+/*echo "<br>";
 echo "Controlador: ". $controller;
 echo "<br>";
 echo "Metodo: ". $method;
 echo "<br>";
 echo "Parametros: ".$params;
+*/
